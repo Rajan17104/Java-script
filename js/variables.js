4b3401cf9610a5140d9760fa09c7ec4b5b6c8448
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-10 10:12:15 +0530

    10-4-js

---
+++ js/variables.js
@@ -0,0 +1,68 @@
+/********var*******/
+
+
+// var x=10;
+// var x=20;
+// console.log(x);  //20
+
+// var x;
+// console.log(x); //undefined
+
+// var x=10;
+// var x;
+// console.log(x);  //10
+
+
+// // Global_scope
+// var x=10;
+// {
+//     var x=20;
+//     // Block scope or Local scope
+// }
+
+// console.log(x);
+
+
+
+/********let*******/
+
+
+// let x=10;
+// let x=10;
+// console.log(x); //error
+
+// let x;
+// let x = 10;
+// console.log(x);  //error
+
+// let x=10;
+// {
+//     let x=20;
+// }
+// console.log(x); //10
+
+// let x=10;
+// x = x + 2;
+
+// console.log(x); //12
+
+
+
+/*******const********/
+
+
+// const x=10;
+// const x=20;
+// console.log(x);  //error
+
+
+// const x;
+// const x=10;
+// console.log(x); //error
+
+// const x=10;
+// {
+//      const x=20;
+// }
+
+// console.log(x);
