7ebe7b3f48fa75f3f82f0e964ad44f47d4ce771e
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-11 09:55:19 +0530

    11-4-marksheet

---
+++ js/operator.js
@@ -0,0 +1,35 @@
+// let x =parseInt(prompt("please enter a value x:"));
+// let y =parseIn/t(prompt("please enter a value y:"));
+
+// let z;
+
+// z = x+=y;
+
+// z = x / y;
+
+// z = x % y;
+
+// z = x ** y;
+
+// z = x*=y;
+
+
+// console.log(z);
+
+// let x =parseInt(prompt("please enter a value x:"));
+// let y =parseInt(prompt("please enter a value y:"));
+
+// let z;
+
+// let x=10, y=20,z="30";
+
+// x=x+y+z;
+
+// console.log(x);
+
+
+let x="10"; y=20; z=30;
+
+x = x+y+z;
+
+console.log(x);
---
+++ js/output.js
@@ -0,0 +1,17 @@
+alert("Hello");
+
+document.getElementById("titel").innerHTML = "Rajan"
+document.getElementById("titel").style.color = "blue"
+
+
+let x = parseInt(prompt("please enter a value of x:"));
+let y = parseInt(prompt("please enter a value of y:"));
+
+let z;
+
+z = x + y;
+
+
+
+
+console.log(z);
