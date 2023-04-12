c1fa65bc3270aa931135f85f1d916017adc94b67
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-12 11:01:53 +0530

    12-4-random

--- js/random.js
+++ js/random.js
@@ -1,5 +1,7 @@
 
-document.getElementById("one").innerHTML = Math.floor(Math.random() * 10);
+ document.getElementById("one").innerHTML = Math.floor(Math.random() * 10);
+
 document.getElementById("two").innerHTML = Math.floor(Math.random() * 10);
 
 
+console.log(x);
