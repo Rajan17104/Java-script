cafd62837a0ca43b5a8315c7e255b1aebbed10ca
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-12 10:04:31 +0530

    12-4-random

--- js/operator.js
+++ js/operator.js
@@ -28,8 +28,51 @@
 // console.log(x);
 
 
-let x="10"; y=20; z=30;
+// let x="10"; y=20; z=30;
 
-x = x+y+z;
+// x = x+y+z;
 
-console.log(x);
+// console.log(x);
+
+
+/******Operator Description*******/
+
+// let x=10 , y="10";
+
+
+// console.log(x==y);
+
+// console.log(x===y);
+
+// console.log(x!=y); 
+
+// console.log(x!==y); 
+
+// console.log(x<y);
+
+// console.log(x>y); 
+
+ 
+
+    /******Logical Operator*******/
+
+// let x=10 , y=20;
+
+// x < y && x!=y ? console.log("true") : console.log("false");
+
+//  !( x > y )? console.log("true") : console.log("false");
+
+
+/*******Type operator******/
+
+// let x="10";
+
+// console.log(typeof(x));
+
+// let x;
+
+// console.log(typeof(x));
+
+let x=true;
+
+console.log(typeof(x));
---
+++ js/random.js
@@ -0,0 +1,5 @@
+
+document.getElementById("one").innerHTML = Math.floor(Math.random() * 10);
+document.getElementById("two").innerHTML = Math.floor(Math.random() * 10);
+
+
