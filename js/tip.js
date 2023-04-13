789c091bfc4d46f1b0570135857311373bf71096
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-13 19:33:32 +0530

    13-4-tip

---
+++ js/tip.js
@@ -0,0 +1,18 @@
+function data(){
+    let bill= parseInt(document.getElementById("bill").value);
+    let per= parseInt(document.getElementById("per").value);
+
+    let ans;
+    ans = bill*per/100;
+
+    let total;
+    
+    total = bill + ans;
+
+    document.getElementById("tip").value = ans;
+    document.getElementById("total").value = total;
+
+    document.getElementById("tip").disabled = true;
+    document.getElementById("total").disabled = true;
+}
+
