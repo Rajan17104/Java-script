efecb271e6aa8a7279b438aec80a6ced44e08e46
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-16 17:07:45 +0530

    14-4-trip

---
+++ js/counter.js
@@ -0,0 +1,21 @@
+
+let x = 0;
+
+
+
+const add = () =>{
+
+    x = x + 1;
+
+    document.getElementById("count").innerHTML = x;
+
+}
+
+
+const sub = () =>{
+
+    x = x - 1;
+
+    document.getElementById("count").innerHTML = x;
+
+}
---
+++ js/country.js
@@ -0,0 +1,37 @@
+const select = () =>{
+
+document.getElementById("ind").value;
+document.getElementById("aus").value;
+document.getElementById("canada").value;
+document.getElementById("us").value;
+
+let ans=document.getElementById("country").value;
+console.log(ans);
+// console.log(i);
+// console.log(a);
+// console.log(c);
+
+if(ans === "India"){
+
+    document.getElementById("main").style.backgroundColor="orange";
+    document.getElementById("msg").innerHTML = "your selected country is India";
+
+}else if(ans === "Australia"){
+
+    document.getElementById("main").style.backgroundColor="yellow";
+    document.getElementById("msg").innerHTML = "your selected country is Australia";
+
+}else if(ans === "Canada"){
+
+    document.getElementById("main").style.backgroundColor="red";
+    document.getElementById("msg").innerHTML ="your selected country is Canada";
+
+}else if(ans === "Us"){
+
+    document.getElementById("main").style.backgroundColor="blue";
+    document.getElementById("msg").innerHTML = "your selected country is Us";
+}
+
+
+}
+
