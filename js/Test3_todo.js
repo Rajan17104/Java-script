b76cb0d08603cd669844f91e4fef04eb9b1f6b9a
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-30 15:31:31 +0530

    30-4-program

---
+++ js/Test3_todo.js
@@ -0,0 +1,33 @@
+arr = [];
+
+
+const todoData = () =>{
+
+// console.log(arr);
+
+let val = document.getElementById("todo").value;
+
+console.log(val);
+
+document.getElementById("ans").innerHTML = val;
+document.getElementById("ans").style.display = 'block';
+document.getElementById("i").style.display = 'block'
+
+arr.push(val);
+console.log(arr);
+
+ let a ;
+ a= a + arr;
+
+parseInt(document.getElementById("ans").innerHTML  = a);
+
+console.log(a);
+
+}
+
+const remove = () =>{
+
+    document.getElementById("ans").innerHTML = '';
+    document.getElementById("i").innerHTML = '';
+
+}
