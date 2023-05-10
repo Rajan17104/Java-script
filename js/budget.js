3af42bebade0748430d91da9c6deb4b69ebb4432
swatidetroja <guddurani.294@gmail.com>
2023-05-10 09:52:20 +0530

    10-5-budget

--- js/budget.js
+++ js/budget.js
@@ -34,15 +34,6 @@ const handleonload = () => {
 
     /*Balance*/
 
-    // let balanceElem = document.createElement("p");
-    // let balTaxElem = document.createTextNode("$0");
-
-    // balanceElem.appendchild(balTaxElem); 
-
-    // let balanceref = document.getElementById("balance");
-
-    // balanceref.appendChild(balanceElem);
-
     let balElem = document.createElement("p");
     let balTaxElem = document.createTextNode("$0");
 
@@ -72,34 +63,45 @@ const handlesubmitExe = () =>{
 
     let exeName = document.getElementById("exe1").value;
     let exeCost = document.getElementById("exe2").value;
-
     console.log(exeName,exeCost);
+    
+    let random = Math.floor(Math.random() * 100);
+    console.log(random);
 
     let trref = document.createElement("tr");
+    trref.setAttribute("id","row" + random);
+
     let td1ref = document.createElement("td");
     let td2ref = document.createElement("td");
+    let td3ref =document.createElement("td");
     let button1 = document.createElement("button");
     let button2 = document.createElement("button");
+    button2.setAttribute("onclick","handleRemove("+ random +")");
     
     let td1Tax = document.createTextNode(exeName);
     let td2Tax = document.createTextNode(exeCost);
     let btn1 = document.createTextNode("Update");
     let btn2 = document.createTextNode("Remove");
 
+    td3ref.appendChild(button1);
+    td3ref.appendChild(button2);
+
     td1ref.appendChild(td1Tax);
     td2ref.appendChild(td2Tax);
     button1.appendChild(btn1);
     button2.appendChild(btn2);
 
+    trref.appendChild(td1ref);
+    trref.appendChild(td2ref);
+    trref.appendChild(td3ref);
+
+
     let exeref = document.getElementById("Tabledata");
 
     exeref.appendChild(trref);
-    exeref.appendChild(td1ref);
-    exeref.appendChild(td2ref);
-    exeref.appendChild(button1);
-    exeref.appendChild(button2);
 
     expense.push({
+        id:random,
         name:exeName,
         cost:exeCost
     });
@@ -107,7 +109,7 @@ const handlesubmitExe = () =>{
     console.log(expense);
 
     handledata();
-
+    
     event.preventDefault();
 
 }
@@ -119,7 +121,9 @@ const handledata = () =>{
     let exCO = 0;
     let total = 0;
 
-    expense.map((v)=> total= total + parseInt(v.cost));
+    expense.map((v)=> {
+        total = total + parseInt(v.cost)
+    });
 
     document.getElementById("expense").innerHTML = total;
 
@@ -128,14 +132,31 @@ const handledata = () =>{
     document.getElementById("balance").innerHTML = exCO;
 
     if(val < total){
-        alert("-- Sorry -- Invalid Expenese" )
+        alert("-- Sorry -- Invalid Expenese Amount")
     }
+
+    document.getElementById("table").style.display="block"
+
+}
+
+const handleRemove = (random) =>{
+    console.log(random);
+
+    let trref = document.getElementById("row"+ random);
+    console.log(trref);
+
+    trref.remove();
+
+    let fData = expense.filter((v,i) => v.id != random);
+
+    expense = [...fData];
+
+    console.log(fData);
 
 }
 
 exeref.addEventListener("submit",handlesubmitExe);
 calref.addEventListener("submit",handlesubmit);
-
 
 
 window.onload = handleonload;
